04b1e88ec8fe8a6c323e349a2402a01edda5f647
Asad Sabri <asad.sabri19@gmail.com>
2024-04-02 23:59:09 +0500

    again check contact

--- src/components/contactus/Contactus.js
+++ src/components/contactus/Contactus.js
@@ -153,7 +153,7 @@ export default function Contactus() {
                   </div>
                   <div className="contact-map">
                     <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5911.808310783564!2d74.43975559163924!3d31.592656892443884!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391911a59288f2c3%3A0x3cf71bbff9333b48!2sAl%20Raheem%20Garden%20Phase%205!5e0!3m2!1sen!2s!4v1712001985089!5m2!1sen!2s"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3643.833618785371!2d90.41265112695315!3d24.036931700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755db9832166e63%3A0x96050e560e6dc4fa!2sBOF%20Central%20Mosque!5e0!3m2!1sen!2sbd!4v1695023265917!5m2!1sen!2sbd"
                       frameBorder="0"
                       allowFullScreen=""
                       aria-hidden="false"
